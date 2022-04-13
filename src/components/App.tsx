7dc7eb890dc7c6763bf9d35bd0f0dfeb315a7310
Ajdin Ahmic <ajdinnahmic@gmail.com>
2022-04-13 14:48:48 +0200

    A little code refactor

---
+++ src/components/App.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Details from './Details'
+import TaskList from './TaskList'
+import EditTask from './EditTask'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import '../css/bootstrap.min.css'
+import '../css/index.css'
+import AddTask from './AddTask'
+
+const TodoList = [
+    {
+        id: 1,
+        task: 'Clean the house',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui rerum assumenda quis debitis vitae error deserunt impedit, dicta omnis!',
+        done: 100
+    },
+    {
+        id: 2,
+        task: 'Do my homework',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, corrupti omnis sunt distinctio est saepe?',
+        done: 50
+    }
+]
+
+
+const App = (): JSX.Element =>
+{
+    let [active, setActive] = useState(Number);
+    let [todo, setTodo] = useState(TodoList);
+    let [open, setOpen] = useState(false);
+    let [openEdit, setOpenEdit] = useState(false);
+
+    return (
+        <div className="container ">
+            <div className="row">
+
+                <h3 className="mainTitle">
+                    TODO APP <span><AddCircleOutlineIcon onClick={() => setOpen(true)} className="cursor s"></AddCircleOutlineIcon></span>
+                </h3>
+                <br></br>
+
+                <AddTask open={open} setOpen={setOpen} todo={todo} />
+                {openEdit === true ? <EditTask setTodo={setTodo} active={active} todo={todo} openEdit={openEdit} setOpenEdit={setOpenEdit} /> : null}
+
+                <TaskList setActive={setActive} active={active} todo={todo} />
+
+                <div className="col-lg-1">
+                </div>
+
+                {active > 0 ? <Details openEdit={openEdit} setOpenEdit={setOpenEdit} setActive={setActive} setTodo={setTodo} todo={todo} active={active} /> : null}
+
+            </div>
+        </div>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react'
 import ReactDOMClient from 'react-dom/client'
-import Details from './details'
-import TaskList from './taskList'
-import EditTask from './editTask'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import './css/bootstrap.min.css'
-import './css/index.css'
-import AddTask from './addTask'
-
-const TodoList = [
-    {
-        id: 1,
-        task: 'Ocistiti kucu',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui rerum assumenda quis debitis vitae error deserunt impedit, dicta omnis!',
-        done: 100
-    },
-    {
-        id: 2,
-        task: 'Heading 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, corrupti omnis sunt distinctio est saepe?',
-        done: 50
-    }
-]
-
-
-const App = (): JSX.Element =>
-{
-    let [active, setActive] = useState(Number);
-    let [todo, setTodo] = useState(TodoList);
-    let [open, setOpen] = useState(false);
-    let [openEdit, setOpenEdit] = useState(false);
-
-    return (
-        <div className="container ">
-            <div className="row">
-
-                <h3 className="mainTitle">
-                    TODO APP <span><AddCircleOutlineIcon onClick={() => setOpen(true)} className="cursor s"></AddCircleOutlineIcon></span>
-                </h3>
-                <br></br>
-                <AddTask open={open} setOpen={setOpen} todo={todo} />
-                {openEdit === true ? <EditTask setTodo={setTodo} active={active} todo={todo} openEdit={openEdit} setOpenEdit={setOpenEdit} /> : null}
-                <TaskList setActive={setActive} active={active} todo={todo} />
-
-                <div className="col-lg-1">
-                </div>
-
-                {active > 0 ? <Details openEdit={openEdit} setOpenEdit={setOpenEdit} setActive={setActive} setTodo={setTodo} todo={todo} active={active} /> : null}
-
-            </div>
-        </div>
-    )
-}
+import App from './components/App'
 
 const root = ReactDOMClient.createRoot(document.querySelector("#root") as HTMLElement);
 root.render(<App />);
